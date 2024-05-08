2b4d93ccf259cc9745b7d9a68baa32f854dd3c4f
Michael Sousa <msousacode@gmail.com>
2024-05-07 23:35:09 -0300

    inicial commit

---
+++ src/main/java/br/com/msousa/testeautomatizado/TesteautomatizadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.msousa.testeautomatizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteautomatizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteautomatizadoApplication.class, args);
+	}
+
+}
