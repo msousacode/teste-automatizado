d4994ba53a47c678ff7b508aa5e23c0692436a14
Michael Sousa <msousacode@gmail.com>
2024-05-08 22:13:21 -0300

    chore: hoje aprendi como abrir um select e selecionar uma opção.

--- src/test/java/br/com/msousa/testeautomatizado/commom/BaseTest.java
+++ src/test/java/br/com/msousa/testeautomatizado/commom/BaseTest.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class BaseTest {
 
+    private By ordenarDrop;
+    private By titleText;
     private WebDriver webDriver;
 
     @BeforeMethod
@@ -20,18 +25,31 @@ public class BaseTest {
         webDriver = new FirefoxDriver();
         webDriver.manage().window().maximize();
         webDriver.navigate().to("https://www.saucedemo.com/");
-
-        TempoUtil.aguardar5Min();
     }
 
-    @Test
+    @Test(priority = 1)
     public void logarPortal() {
         webDriver.findElement(By.name("user-name")).sendKeys("standard_user");
         TempoUtil.aguardar3Min();
+        //webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(60000));
         webDriver.findElement(By.name("password")).sendKeys("secret_sauce");
         TempoUtil.aguardar3Min();
-        webDriver.findElement(By.name("login-button")).click();
 
-        TempoUtil.aguardar5Min();
+        webDriver.findElement(By.name("login-button")).click();
+        TempoUtil.aguardar3Min();
+
+        /**
+         * Exemplo de como abrir um elemento dropDown e selecionar um valor.
+         *
+         * Aqui foi utilizado o exemplo selecionando por index, mas poderia ser pela texto visível ou pelo value.
+         */
+        webDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/div/span/select")).click();
+        TempoUtil.aguardar3Min();
+
+        Select select = new Select(webDriver.findElement(By.xpath("/html/body/div/div/div/div[1]/div[2]/div/span/select")));
+        select.selectByIndex(2);
+        //Exemplo usando o texto visível.
+        //select.selectByVisibleText("Price (high to low)");
+
     }
 }
